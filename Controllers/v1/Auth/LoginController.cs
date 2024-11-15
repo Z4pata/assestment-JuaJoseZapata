5bd2d420a7bda8276c407902ab9affada941e172
Z4pata <zapata.devs@gmail.com>
2024-11-15 08:49:17 -0500

    Feat: Login logic

---
+++ Controllers/v1/Auth/LoginController.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using assestment_JuaJoseZapata.Config;
+using assestment_JuaJoseZapata.DTOs.Request;
+using Microsoft.AspNetCore.Mvc;
+
+namespace assestment_JuaJoseZapata.Controllers.v1.Auth
+{
+    public partial class AuthController
+    {
+         [HttpPost]
+        public async Task<IActionResult> Login (LoginRequest request)
+        {
+            var user = await _service.GetByEmail(request.Email);
+
+            if (user == null)
+            {
+                return NotFound("User by that credentials Not found");
+            }
+
+            if (!_service.CheckPassword(request.Password, user))
+            {
+                return BadRequest("Incorrect credentials");
+            }
+
+            var token = JWT.GenerateJwtToken(user);
+
+            return Ok($"Logeado con exito, su token es: {token}");
+        }
+    }
+}
